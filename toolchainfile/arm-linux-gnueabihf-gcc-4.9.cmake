5740c0944b69c97fb9a084032332a5843147a019
Andreas Kunz <andreas.kunz@ntb.ch>
2019-10-21 15:28:41 +0200

    Fix problem that occurs when cross-compiling
    When cross-compiling the googletest framework, cmake throws
    an error. This workaround avoids the problem.

--- toolchainfile/arm-linux-gnueabihf-gcc-4.9.cmake
+++ toolchainfile/arm-linux-gnueabihf-gcc-4.9.cmake
@@ -9,3 +9,9 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
+# Workaround to fix issue:
+# CMake Error: TRY_RUN() invoked in cross-compiling mode, please set the
+# following cache variables appropriately: THREADS_PTHREAD_ARG (advanced)
+# see: https://gitlab.kitware.com/cmake/cmake/issues/16920
+set(THREADS_PTHREAD_ARG "2" CACHE STRING "Forcibly set by arm-linux-gnueabihf-gcc-4.9.cmake." FORCE)
+
