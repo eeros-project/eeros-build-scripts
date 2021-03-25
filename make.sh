5728ef5318a38fcccab01a3a7ca5ccc5da024b62
ursgraf <urs.graf@ntb.ch>
2021-03-25 15:45:37 +0100

    Change for cb20

--- make.sh
+++ make.sh
@@ -19,10 +19,6 @@ function build ()
   shift 2
   local flags="$@"
 
-  if [ "$use_toolchain_file" = true ]; then
-    flags="-DCMAKE_TOOLCHAIN_FILE=$toolchain_file $flags"
-  fi
-
   mkdir -p "$build_dir"
   pushd "$build_dir"
 
@@ -32,76 +28,30 @@ function build ()
         "$source_dir"
 
   $MAKE
-  $MAKE install
   popd
 }
 
 
-if [ "$use_cross_compilation_environment" = true ]; then
+#if [ "$use_cross_compilation_environment" = true ]; then
+  unset LD_LIBRARY_PATH
   . "$environment_setup_script"
-fi
+  install_dir="$SDKTARGETSYSROOT/$sdk_install_dir"
+  echo "Due to cross compilation environment, install_dir is set to: $install_dir"
+#fi
 
 
-if [ "$use_ros_setup_script" = true ]; then
+if [ "$use_ros" = true ]; then
   . "$ros_setup_script"
 fi
 
 
 # workaround to correctly setup environment when both of them are used
-if [ "$use_cross_compilation_environment" = true -a "$use_ros_setup_script" = true ]; then
+if [ "$use_cross_compilation_environment" = true -a "$use_ros" = true ]; then
   unset LD_LIBRARY_PATH
   . "$environment_setup_script"
 fi
 
 
-if [ "$use_can" = true ]; then
-  build "$canopenlib_source_dir" "$canopenlib_build_dir"
-fi
-
-
-build "$eeros_source_dir" "$eeros_build_dir" "-DUSE_ROS=$use_ros -DUSE_CAN=$use_can"
-
-
-if [ "$use_flink" = true ]; then
-  build "$flinklib_source_dir" "$flinklib_build_dir"
-  build "$flink_eeros_source_dir" "$flink_eeros_build_dir" -DREQUIRED_EEROS_VERSION="$eeros_required_version"
-fi
-
-
-if [ "$use_bbblue" = true ]; then
-  build "$bbblue_eeros_source_dir" "$bbblue_eeros_build_dir" -DADDITIONAL_INCLUDE_DIRS="$librobotcontrol_source_dir/libraries/" \
-                                                            -DADDITIONAL_LINK_DIRS="$librobotcontrol_source_dir/libraries/" \
-                                                            -DREQUIRED_EEROS_VERSION="$eeros_required_version"
-fi
-
-
-if [ "$use_comedi" = true ]; then
-  build "$comedi_eeros_source_dir" "$comedi_eeros_build_dir" -DREQUIRED_EEROS_VERSION="$eeros_required_version"
-fi
-
-
-if [ "$use_simulator" = true ]; then
-  build "$sim_eeros_source_dir" "$sim_eeros_build_dir" -DREQUIRED_EEROS_VERSION="$eeros_required_version"
-fi
-
-
-if [ "$use_ros" = true ]; then
-  build "$ros_eeros_source_dir" "$ros_eeros_build_dir" -DREQUIRED_EEROS_VERSION="$eeros_required_version"
-fi
-
-
 if [ "$use_custom_application" = true ]; then
-  build "$custom_application_source_dir" "$custom_application_build_dir" -DUSE_SIM="$use_simulator" \
-                                                                         -DUSE_FLINK="$use_flink" \
-                                                                         -DUSE_BBBLUE="$use_bbblue" \
-                                                                         -DUSE_COMEDI="$use_comedi" \
-                                                                         -DUSE_ROS="$use_ros" \
-                                                                         -DUSE_CAN="$use_can" \
-                                                                         -DREQUIRED_EEROS_VERSION="$eeros_required_version" \
-                                                                         -DREQUIRED_SIM_EEROS_VERSION="$sim_eeros_required_version" \
-                                                                         -DREQUIRED_FLINKLIB_VERSION="$flinklib_required_version" \
-                                                                         -DREQUIRED_FLINK_EEROS_VERSION="$flink_eeros_required_version" \
-                                                                         -DREQUIRED_BBBLUE_EEROS_VERSION="$bbblue_eeros_required_version" \
-                                                                         -DREQUIRED_COMEDI_EEROS_VERSION="$comedi_eeros_required_version" \
-                                                                         -DREQUIRED_ROS_EEROS_VERSION="$ros_eeros_required_version"
+  build "$custom_application_source_dir" "$custom_application_build_dir" -DREQUIRED_EEROS_VERSION="$eeros_required_version" 
 fi
